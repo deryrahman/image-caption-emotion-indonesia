5353a89689b1267e26d55661428c86f6cb3d2d92
Dery Rahman A <dery.ra@gmail.com>
2019-02-26 22:53:59 +0700

    add resnet152 from keras

--- model.py
+++ model.py
@@ -1,26 +1,77 @@
-from tensorflow.contrib import slim
-from tensorflow.contrib.slim.nets import resnet_v2
+from keras.applications.resnet_v2 import ResNet152V2
+from keras.layers import Input, TimeDistributed, Dense, LSTM, Embedding, concatenate
+from keras.models import Model
 
 IMAGE_SIZE = 224
 NUM_CLASSES = 1001
 
 
+class NIC():
+
+    def __init__(self,
+                 token_len,
+                 vocab_size,
+                 num_image_features=2048,
+                 hidden_size=512,
+                 embedding_size=512):
+        self.token_len = token_len
+        self.vocab_size = vocab_size
+        self.num_image_features = num_image_features
+        self.hidden_size = hidden_size
+        self.embedding_size = embedding_size
+        self.model = None
+
+    def build(self):
+        # image embedding
+        image_input = Input(
+            shape=(1, self.num_image_features), name='image_input')
+        image_embedding = Dense(
+            self.embedding_size, name='image_embedding')(image_input)
+
+        # word embedding
+        word_input = Input(shape=(self.token_len,), name='word_input')
+        word_embedding = Embedding(
+            input_dim=self.vocab_size,
+            output_dim=self.embedding_size,
+            name='word_embedding')(word_input)
+
+        # concatenate
+        embedding = concatenate([image_embedding, word_embedding],
+                                axis=1,
+                                name='embedding')
+
+        # decoder LSTM
+        decoder_lstm = LSTM(
+            self.hidden_size, return_sequence=True,
+            name='decoder_lstm')(embedding)
+        output = TimeDistributed(
+            Dense(self.vocab_size, activation='softmax',
+                  name='output'))(decoder_lstm)
+
+        self.model = Model(inputs=[image_input, word_input], outputs=output)
+
+    def get_model(self):
+        if not self.model:
+            self.build()
+        return self.model
+
+
 class EncoderResNet152():
 
-    def __init__(self):
-        self.checkpoint = "./pretrained/resnet_v2_152_2017_04_14/resnet_v2_152.ckpt"
+    def __init__(self, weights='imagenet'):
+        self.weights = weights
+        self.model = None
 
-    def build(self, preprocessed_images):
-        assert len(preprocessed_images.shape) == 4
-        assert preprocessed_images.shape[1] == IMAGE_SIZE
-        assert preprocessed_images.shape[2] == IMAGE_SIZE
-        assert preprocessed_images.shape[3] == 3
-        with slim.arg_scope(resnet_v2.resnet_arg_scope()):
-            net, end_point = resnet_v2.resnet_v2_152(
-                preprocessed_images, num_classes=NUM_CLASSES, is_training=False)
-        self.init_fn = slim.assign_from_checkpoint_fn(
-            self.checkpoint, slim.get_variables_to_restore())
-        return net, end_point
+    def build(self):
+        # from pretrained model
+        resnet_152 = ResNet152V2(weights=self.weights)
+        self.model = Model(
+            inputs=resnet_152.input, outputs=resnet_152.layers[-2].output)
+
+    def get_model(self):
+        if not self.model:
+            self.build()
+        return self.model
 
 
 class DecoderFactoredLSTM():

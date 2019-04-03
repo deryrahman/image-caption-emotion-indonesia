176a74632ac21d75d50d21630e8fcbcfaaf7288d
Dery Rahman A <dery.ra@gmail.com>
2019-04-03 11:48:47 +0700

    feat(preparation): add function process_caption_index_helper and invoke_emotion_to_dataset

--- preparation.py
+++ preparation.py
@@ -1,5 +1,7 @@
 import json
 import numpy as np
+import pickle
+import os
 
 
 def convert_mongo(path):
@@ -58,13 +60,61 @@ def load_caption(path_folder):
     return (filenames_train, captions_train), (filenames_val, captions_val)
 
 
+def process_caption_index_helper(path, flickr_folder, coco_folder, save_path):
+    if os.path.isfile(save_path):
+        with open(save_path, 'rb') as f:
+            image_id_to_idx = pickle.load(f)
+        return image_id_to_idx
+    image_id_to_filename = {}
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            data = json.loads(line)
+            image_id_to_filename[data['image_id']] = data['file_name']
+    filename_to_idx = {}
+    for folder in [flickr_folder, coco_folder]:
+        with open(folder + '/caption.json', 'r') as f:
+            caption = json.load(f)
+            for i, r in enumerate(caption):
+                filename_to_idx[r['filename']] = i
+    image_id_to_idx = {}
+    for image_id, filename in image_id_to_filename.items():
+        image_id_to_idx[image_id] = filename_to_idx[filename]
+    with open(save_path, 'wb') as f:
+        pickle.dump(image_id_to_idx, f)
+    return image_id_to_idx
+
+
+def invoke_emotion_to_dataset(mongo_dump_path, image_id_to_idx, dataset_folder,
+                              dataset, emotion):
+    if dataset != 'flickr' and dataset != 'coco':
+        raise ValueError('dataset only flickr or coco')
+    with open(dataset_folder + '/caption.json', 'r') as f:
+        caption = json.load(f)
+    with open(mongo_dump_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            user = json.loads(line)
+            for user_caption in user['captions']:
+                if user_caption['step'] != 'emotion' or user_caption[
+                        'captionEmotion'][emotion] == '':
+                    continue
+                if user_caption['image_id'].split('-')[0] != dataset:
+                    continue
+                caption[image_id_to_idx[user_caption['image_id']]]['emotions'][
+                    emotion] = user_caption['captionEmotion'][emotion]
+    with open(dataset_folder + '/caption.json', 'w') as f:
+        json.dump(caption, f)
+    return caption
+
+
 if __name__ == '__main__':
     path = './dataset'
     flickr_folder = path + '/flickr10k'
-    coco_folder = path + '/mscoco10k'
+    coco_folder = path + '/mscoco'
 
     # convert mongo to caption dict
-    caption_flickr, caption_coco = convert_mongo(path=path + 'caption_bc.json')
+    caption_flickr, caption_coco = convert_mongo(path=path + '/caption_bc.json')
 
     # save
     with open(flickr_folder + '/caption.json', 'w') as f:

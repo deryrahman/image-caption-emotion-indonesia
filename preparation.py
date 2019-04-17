a5ae02cb5698a5b7dd970924a60667d5b4a3df86
Dery Rahman A <dery.ra@gmail.com>
2019-04-17 21:24:04 +0700

    feat(prepartion): update preparation preprocess, for spliting dataset

--- preparation.py
+++ preparation.py
@@ -1,18 +1,40 @@
+from tokenizer import mark_captions, flatten, TokenizerWrap
 import json
-import numpy as np
-import pickle
 import os
 
 
 def convert_mongo(path):
+    """Convert mongo dump from imagecaption.geekstudio.id into list of captions
+
+    Arguments:
+        path {str} -- string of path for mongo dump
+
+    Returns:
+        list pair -- caption_flickr, caption_coco. Each list handle list
+        of captions with format:
+        {
+            'image_id': ...,
+            'filename': ...,
+            'captions': [
+                ...,
+                ...
+            ],
+            'emotions': {
+                'happy': ...,
+                'sad': ...,
+                'angry': ...
+            }
+        }
+    """
+
     caption_flickr = []
     caption_coco = []
-    # captions.json is a mongoexport from imagecaption.geekstudio.id
     with open(path) as f:
         content = f.readlines()
         for d in content:
             tmp = json.loads(d.rstrip())
             data = {
+                'image_id': tmp['image_id'],
                 'filename': tmp['file_name'],
                 'captions': tmp['captions'],
                 'emotions': {}
@@ -24,91 +46,239 @@ def convert_mongo(path):
     return caption_flickr, caption_coco
 
 
-def split_and_save(captions, path_folder, seed=0, train_num=9000):
-    n = len(captions)
-    indexes = np.array([v['filename'].split('.')[0] for v in captions])
-    np.random.seed(seed)
-    np.random.shuffle(indexes)
-    train_indexes = indexes[-train_num:,]
-    validation_indexes = indexes[:n - train_num,]
+def split_dataset(filenames,
+                  captions,
+                  path_folder,
+                  tokenizer,
+                  val_percentage=10,
+                  test_percentage=10):
+    """Split given list of filenames and lisf of captions into train, val, and test set.
+    We need to have all word in training data. So before it, we need to do heuristic function,
+    to sort the captions based on word frequency count. This can be done with
+    _sort_captions_by_largest_word_freq function. Then, iterate each caption, if words in that caption
+    have frequency larger than 1, then it's safe to take the caption as validation or test set.
+    This can be done with _split_train_val_test function
+
+    Arguments:
+        filenames {list} -- list of filenames. eg ['123.jpg', '124.jpg', '448.jpg']
+        captions {list of list} -- list of captions. eg [
+            ['1 ref a', '1 ref b', '1 ref c'], ['2 ref a', '2 ref b', '2 ref c']
+            ]
+        path_folder {str} -- folder path to save the result
+        tokenizer {tokenizer} -- tokenizer object
+
+    Keyword Arguments:
+        val_percentage {int} -- validation percentage (default: {10})
+        test_percentage {int} -- test percentage (default: {10})
+
+    Returns:
+        list, list, list -- list of train indexes, val indexes, and test indexes
+    """
+
+    val_num = len(captions) * val_percentage // 100
+    test_num = len(captions) * test_percentage // 100
+    pair_all_freq_ids = _sort_captions_by_largest_word_freq(captions, tokenizer)
+    train_ids, val_ids, test_ids = _split_train_val_test(
+        pair_all_freq_ids, captions, tokenizer, val_num, test_num)
+
+    train_indexes = [filenames[i].split('.')[0] for i in train_ids]
+    val_indexes = [filenames[i].split('.')[0] for i in val_ids]
+    test_indexes = [filenames[i].split('.')[0] for i in test_ids]
+
+    return train_indexes, val_indexes, test_indexes
+
+
+def save_dataset(filenames, captions, path_folder, train_indexes, val_indexes,
+                 test_indexes):
+    """Save dataset that has been splitted into train, val, and test
+
+    Arguments:
+        path_folder {str} -- string of path dataset folder. eg ./dataset/flickr10k/factual
+        filenames {list} -- list of filename
+        captions {list of list} -- list of captions
+        train_indexes {list} -- list of train indexes
+        val_indexes {list} -- list of validation indexes
+        test_indexes {test} -- list of test indexes
+    """
+
+    if not os.path.isdir(path_folder):
+        os.mkdir(path_folder)
+
+    with open(path_folder + '/filenames.json', 'w') as f:
+        json.dump(filenames, f)
+    with open(path_folder + '/captions.json', 'w') as f:
+        json.dump(captions, f)
     with open(path_folder + '/train.txt', 'w') as f:
         f.write('\n'.join(train_indexes))
     with open(path_folder + '/val.txt', 'w') as f:
-        f.write('\n'.join(validation_indexes))
+        f.write('\n'.join(val_indexes))
+    with open(path_folder + '/test.txt', 'w') as f:
+        f.write('\n'.join(test_indexes))
 
 
-def load_caption(path_folder, lang='id', modes=['happy', 'sad', 'angry']):
-    filenames_train = {mode: () for mode in modes}
-    filenames_train.update({'factual': ()})
-    captions_train = {mode: () for mode in modes}
-    captions_train.update({'factual': ()})
-    filenames_val = {mode: () for mode in modes}
-    filenames_val.update({'factual': ()})
-    captions_val = {mode: () for mode in modes}
-    captions_val.update({'factual': ()})
-    with open(path_folder + '/caption.json', 'r') as f:
-        data = json.load(f)
-        with open(path_folder + '/train.txt', 'r') as f2:
-            train_indexes = [d.rstrip() for d in f2.readlines()]
-        with open(path_folder + '/val.txt', 'r') as f2:
-            validation_indexes = [d.rstrip() for d in f2.readlines()]
-        for d in data:
-            idx = d['filename'].split('.')[0]
-            filename = d['filename']
-            if idx in train_indexes:
-                filenames_train['factual'] += (filename,)
-                captions_train['factual'] += ([
-                    cap[lang] for cap in d['captions']
-                ],)
-                for mode in modes:
-                    caption_with_mode = d['emotions'].get(mode)
-                    if caption_with_mode is not None:
-                        filenames_train[mode] += (filename,)
-                        captions_train[mode] += ([caption_with_mode],)
-            elif idx in validation_indexes:
-                filenames_val['factual'] += (filename,)
-                captions_val['factual'] += ([
-                    cap[lang] for cap in d['captions']
-                ],)
-                for mode in modes:
-                    caption_with_mode = d['emotions'].get(mode)
-                    if caption_with_mode is not None:
-                        filenames_val[mode] += (filename,)
-                        captions_val[mode] += ([caption_with_mode],)
-    return (filenames_train, captions_train), (filenames_val, captions_val)
+def _sort_captions_by_largest_word_freq(captions, tokenizer):
+    """Sort captions based on word count frequency within captions.
+    It uses for splitting heuristic, because we need to have all word in training dataset.
+    This function is used to sort the caption from largest word count frequency.
+
+    So with this frequency heuristic, we can split validation and test
+
+    Arguments:
+        captions {list of list} -- list of captions
+        tokenizer {tokenizer} -- tokenizer object
+
+    Returns:
+        list of tuple -- pair of frequency and id (freq,id)
+    """
+
+    pair_freq_ids = []
+
+    for i in range(len(captions)):
+        caps = captions[i]
+        total = 0
+        length = 0
+        for cap in tokenizer.texts_to_sequences(caps):
+            for word_i in cap:
+                length += 1
+                word = tokenizer.index_to_word[word_i]
+                total += tokenizer.word_counts[word]
+        pair_freq_ids.append((total / length, i))
+    pair_freq_ids = sorted(pair_freq_ids, key=lambda x: x[0], reverse=True)
+    return pair_freq_ids
 
 
-def process_caption_index_helper(path, flickr_folder, coco_folder, save_path):
-    if os.path.isfile(save_path):
-        with open(save_path, 'rb') as f:
-            image_id_to_idx = pickle.load(f)
-        return image_id_to_idx
-    image_id_to_filename = {}
-    with open(path, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            data = json.loads(line)
-            image_id_to_filename[data['image_id']] = data['file_name']
-    filename_to_idx = {}
-    for folder in [flickr_folder, coco_folder]:
-        with open(folder + '/caption.json', 'r') as f:
-            caption = json.load(f)
-            for i, r in enumerate(caption):
-                filename_to_idx[r['filename']] = i
-    image_id_to_idx = {}
-    for image_id, filename in image_id_to_filename.items():
-        image_id_to_idx[image_id] = filename_to_idx[filename]
-    with open(save_path, 'wb') as f:
-        pickle.dump(image_id_to_idx, f)
-    return image_id_to_idx
+def _split_train_val_test(pair_freq_ids, captions, tokenizer, val_num,
+                          test_num):
+    """Split train, validation, and test set. It will calculate the words frequency
+    in each caption. If the words have frequency larger than 1, then it's safe to take it
+    as validation or test set
+
+    Arguments:
+        pair_freq_ids {list of tuple} -- list of tuple frequency and ids. (freq, id)
+        captions {list of list} -- list of captions
+        tokenizer {tokenizer} -- tokenizer object
+        val_num {int} -- how many validation data that need to take
+        test_num {int} -- how many test data that need to take
+
+    Returns:
+        list, list, list -- list of ids for train, validation, and test set
+    """
+
+    word_counts = tokenizer.word_counts
+    train_ids = []
+    val_ids = []
+    test_ids = []
+
+    for total, index in pair_freq_ids:
+        caps = captions[index]
+        is_safe = True
+        for cap in tokenizer.texts_to_sequences(caps):
+            if not is_safe:
+                break
+            for word_i in cap:
+                if not is_safe:
+                    break
+                word = tokenizer.index_to_word[word_i]
+                cnt = word_counts[word]
+                word_counts[word] -= 1
+                is_safe = cnt > 1
+        if is_safe and len(val_ids) < val_num:
+            val_ids.append(index)
+        elif is_safe and len(test_ids) < test_num:
+            test_ids.append(index)
+        else:
+            train_ids.append(index)
+
+    return train_ids, val_ids, test_ids
 
 
-def invoke_emotion_to_dataset(mongo_dump_path, image_id_to_idx, dataset_folder,
-                              dataset, emotion):
+def load_caption(path_folder):
+    """Load caption from folder. The folder must has the structure like this:
+    /filenames.json -- contain list of filenames
+    /captions.json -- contain list of list of caption
+    /train.txt -- contain all train indexes
+    /val.txt -- contain all validation indexes
+    /test.txt -- contain all test indexes
+
+    Arguments:
+        path_folder {str} -- string of path folder
+
+    Returns:
+        tuple, tuple, tuple -- three tuples of train, val, and test.
+        Each tuple has filenames and captions (filename, caption)
+    """
+
+    with open(path_folder + '/filenames.json', 'r') as f:
+        filenames = json.load(f)
+    with open(path_folder + '/captions.json', 'r') as f:
+        captions = json.load(f)
+    with open(path_folder + '/train.txt', 'r') as f:
+        train_indexes = [d.rstrip() for d in f.readlines()]
+    with open(path_folder + '/val.txt', 'r') as f:
+        val_indexes = [d.rstrip() for d in f.readlines()]
+    with open(path_folder + '/test.txt', 'r') as f:
+        test_indexes = [d.rstrip() for d in f.readlines()]
+
+    filenames_train, captions_train = _filter_data_by_indexes(
+        filenames=filenames, captions=captions, indexes=train_indexes)
+    filenames_val, captions_val = _filter_data_by_indexes(
+        filenames=filenames, captions=captions, indexes=val_indexes)
+    filenames_test, captions_test = _filter_data_by_indexes(
+        filenames=filenames, captions=captions, indexes=test_indexes)
+
+    train = (filenames_train, captions_train)
+    val = (filenames_val, captions_val)
+    test = (filenames_test, captions_test)
+    return train, val, test
+
+
+def _filter_data_by_indexes(filenames, captions, indexes):
+    """This function is used to filter the files and captions based on the indexes
+
+    Arguments:
+        filenames {list} -- list of filenames
+        captions {list of list} -- list of list of captions
+        indexes {list} -- list of indexes
+
+    Returns:
+        list, list -- filtered filenames and filtered captions
+    """
+
+    filtered_filenames = ()
+    filtered_captions = ()
+    for filename, caption in zip(filenames, captions):
+        index = filename.split('.')[0]
+        if index in indexes:
+            filtered_filenames += (filename,)
+            filtered_captions += ([cap for cap in caption],)
+    return filtered_filenames, filtered_captions
+
+
+def invoke_emotion_to_dataset(mongo_dump_path, dataset_folder, dataset,
+                              emotion):
+    """This function is used to invoke the emotion into current dataset
+
+    Arguments:
+        mongo_dump_path {str} -- string of path of the original captions.json
+        dataset_folder {str} -- string of dataset folder
+        dataset {str} -- either flickr or coco
+        emotion {str} -- which emotion that you want to invoke
+
+    Raises:
+        ValueError -- raises when dataset neither flickr nor coco
+
+    Returns:
+        list -- list of dict captions
+    """
+
     if dataset != 'flickr' and dataset != 'coco':
         raise ValueError('dataset only flickr or coco')
-    with open(dataset_folder + '/caption.json', 'r') as f:
-        caption = json.load(f)
+
+    with open(dataset_folder + '/captions.json', 'r') as f:
+        captions = json.load(f)
+
+    image_id_to_idx = _generate_image_id_to_ids(dataset_folder)
+
     with open(mongo_dump_path, 'r') as f:
         lines = f.readlines()
         for line in lines:
@@ -119,20 +289,48 @@ def invoke_emotion_to_dataset(mongo_dump_path, image_id_to_idx, dataset_folder,
                     continue
                 if user_caption['image_id'].split('-')[0] != dataset:
                     continue
-                caption[image_id_to_idx[user_caption['image_id']]]['emotions'][
+                captions[image_id_to_idx[user_caption['image_id']]]['emotions'][
                     emotion] = user_caption['captionEmotion'][emotion]
-    with open(dataset_folder + '/caption.json', 'w') as f:
-        json.dump(caption, f)
-    return caption
+
+    with open(dataset_folder + '/captions.json', 'w') as f:
+        json.dump(captions, f)
+
+    return captions
+
+
+def _generate_image_id_to_ids(dataset_folder):
+    """Function to help generate map of image_id and idx map[image_id] = idx
+
+    Arguments:
+        dataset_folder {str} -- string of dataset folder
+
+    Returns:
+        map -- map[image_id] = idx
+    """
+
+    image_id_to_idx = {}
+    with open(dataset_folder + '/captions.json', 'r') as f:
+        contents = json.load(f)
+
+    for i, data in enumerate(contents):
+        image_id_to_idx[data['image_id']] = i
+    return image_id_to_idx
 
 
 def invoke_edited_to_dataset(mongo_dump_path, dataset_folder):
+    """This function is used to invoke edited caption into current dataset
+
+    Arguments:
+        mongo_dump_path {str} -- string of mongo dump path
+        dataset_folder {str} -- string of dataset folder
+    """
+
     mp = {}
     with open(mongo_dump_path, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            test = json.loads(line)
-            for cap in test['captions']:
+            user = json.loads(line)
+            for cap in user['captions']:
                 if len(cap['captionEdit']) == 0:
                     continue
                 for tmp in cap['captionEdit']:
@@ -140,36 +338,75 @@ def invoke_edited_to_dataset(mongo_dump_path, dataset_folder):
                     if mp.get(caption_id) is None:
                         mp[caption_id] = []
                     mp[caption_id].append(tmp['text'])
-    with open(dataset_folder + '/caption.json', 'r') as f:
-        data = json.load(f)
-    for j in range(len(data)):
-        for i, tt in enumerate(data[j]['captions']):
-            caption_id = tt['caption_id']
+
+    with open(dataset_folder + '/captions.json', 'r') as f:
+        contents = json.load(f)
+
+    for i in range(len(contents)):
+        data = contents[i]
+        for j, cap in enumerate(data['captions']):
+            caption_id = cap['caption_id']
             if mp.get(caption_id) is None:
-                data[j]['captions'][i]['edited'] = tt['id']
+                data['captions'][j]['edited'] = cap['id']
             else:
-                data[j]['captions'][i]['edited'] = mp[caption_id][0]
-    with open(dataset_folder + '/caption.json', 'w') as f:
+                data['captions'][j]['edited'] = mp[caption_id][0]
+
+    with open(dataset_folder + '/captions.json', 'w') as f:
         json.dump(data, f)
 
 
 if __name__ == '__main__':
     path = './dataset'
-    flickr_folder = path + '/flickr10k'
-    coco_folder = path + '/mscoco'
+    mongo_dump_path = './dataset/dump/041619.json'
+    flickr_folder = './dataset/flickr10k'
 
     # convert mongo to caption dict
     caption_flickr, caption_coco = convert_mongo(path=path + '/caption_bc.json')
 
     # save
-    with open(flickr_folder + '/caption.json', 'w') as f:
+    with open(flickr_folder + '/captions.json', 'w') as f:
         json.dump(caption_flickr, f)
-    with open(coco_folder + '/caption.json', 'w') as f:
-        json.dump(caption_coco, f)
 
-    split_and_save(caption_flickr, flickr_folder)
-    split_and_save(caption_coco, coco_folder)
+    # invoke new edited caption
+    invoke_edited_to_dataset(mongo_dump_path, flickr_folder)
 
-    train, val = load_caption(flickr_folder)
-    filenames_train, captions_train = train
-    filenames_val, captions_val = val
+    # invoke new emotion
+    for mode in ['happy', 'sad', 'angry']:
+        invoke_emotion_to_dataset(mongo_dump_path, flickr_folder, 'flickr',
+                                  mode)
+
+    # separate filenames and captions
+    all_filenames = {'factual': [], 'happy': [], 'sad': [], 'angry': []}
+    all_captions = {'factual': [], 'happy': [], 'sad': [], 'angry': []}
+    for mode in ['happy', 'sad', 'angry']:
+        for data in caption_flickr:
+            if data['emotions'].get(mode):
+                all_filenames[mode].append(data['filename'])
+                all_captions[mode].append([data['emotions'][mode]])
+    all_filenames['factual'] = [data['filename'] for data in caption_flickr]
+    all_captions['factual'] = [[caption['id']
+                                for caption in data['captions']]
+                               for data in caption_flickr]
+
+    # create tokenizer
+    modes = ['happy', 'sad', 'angry']
+    captions_flat_all = []
+    for mode in ['factual'] + modes:
+        captions_marked = mark_captions(all_captions[mode])
+        captions_flat = flatten(captions_marked)
+        tokenizer = TokenizerWrap(texts=captions_flat)
+        # remove oov words
+        tmp = tokenizer.texts_to_sequences(captions_flat)
+        captions_flat = tokenizer.sequences_to_texts(tmp)
+        captions_flat_all.extend(captions_flat)
+    tokenizer = TokenizerWrap(texts=captions_flat_all)
+
+    # save to folder
+    modes = ['happy', 'sad', 'angry']
+    for mode in ['factual'] + modes:
+        train_indexes, val_indexes, test_indexes = split_dataset(
+            all_filenames[mode], all_captions[mode], flickr_folder + '/' + mode,
+            tokenizer, 10, 10)
+        save_dataset(all_filenames[mode], all_captions[mode],
+                     flickr_folder + '/' + mode, train_indexes, val_indexes,
+                     test_indexes)

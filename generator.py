bed35a2a5951eac6d13ef87d6809a72f9881dd0a
Dery Rahman A <dery.ra@gmail.com>
2019-04-19 22:30:09 +0700

    feat(generator): separate generation with transfer value

--- generator.py
+++ generator.py
@@ -17,29 +17,46 @@ def batch_generator(batch_size,
                     tokens,
                     with_transfer_values=True):
     while True:
-        ids = np.random.randint(len(tokens), size=batch_size)
-        partial_transfer_values = transfer_values[ids]
-
-        partial_tokens = get_random_caption_tokens(ids, tokens)
-
-        max_tokens = np.max([len(t) for t in partial_tokens])
-        tokens_padded = pad_sequences(
-            partial_tokens,
-            maxlen=max_tokens,
-            padding='post',
-            truncating='post')
-
-        decoder_input_data = tokens_padded[:, 0:-1]
-        decoder_output_data = tokens_padded[:, 1:]
-
         if with_transfer_values:
+            ids = np.random.randint(len(tokens), size=batch_size)
+            partial_transfer_values = transfer_values[ids]
+
+            partial_tokens = get_random_caption_tokens(ids, tokens)
+
+            max_tokens = np.max([len(t) for t in partial_tokens])
+            tokens_padded = pad_sequences(
+                partial_tokens,
+                maxlen=max_tokens,
+                padding='post',
+                truncating='post')
+
+            decoder_input_data = tokens_padded[:, 0:-1]
+            decoder_output_data = tokens_padded[:, 1:]
+
             x_data = {
                 'decoder_input': decoder_input_data,
                 'transfer_values_input': partial_transfer_values
             }
+
+            y_data = {'decoder_output': decoder_output_data}
+
+            yield (x_data, y_data)
         else:
+            ids = np.random.randint(len(tokens), size=batch_size)
+            partial_tokens = get_random_caption_tokens(ids, tokens)
+            max_tokens = np.max([len(t) for t in partial_tokens])
+
+            tokens_padded = pad_sequences(
+                partial_tokens,
+                maxlen=max_tokens,
+                padding='post',
+                truncating='post')
+
+            decoder_input_data = tokens_padded[:, 0:-1]
+            decoder_output_data = tokens_padded[:, 1:]
+
             x_data = {'decoder_input': decoder_input_data}
 
-        y_data = {'decoder_output': decoder_output_data}
+            y_data = {'decoder_output': decoder_output_data}
 
-        yield (x_data, y_data)
+            yield (x_data, y_data)

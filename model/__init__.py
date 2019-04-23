9f45561086002650367dd2f1f64a3fd85d26848e
Dery Rahman A <dery.ra@gmail.com>
2019-04-24 00:26:08 +0700

    feat(resnet): remove resnet

--- model/__init__.py
+++ model/__init__.py
@@ -1,9 +1,7 @@
 from .nic import NIC
 from .stylenet import StyleNet
-from .resnet import EncoderResNet152
 from .seq_to_seq import Seq2Seq
 
 NIC = NIC
 StyleNet = StyleNet
-EncoderResNet152 = EncoderResNet152
 Seq2Seq = Seq2Seq

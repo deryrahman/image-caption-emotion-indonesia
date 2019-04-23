7099ca7f8f30ef9bc6c35fe99f8a4792f19a7982
Dery Rahman A <dery.ra@gmail.com>
2019-04-23 22:47:37 +0700

    feat(predictor): generate caption together with seq2seq

--- utils/predictor.py
+++ utils/predictor.py
@@ -1,47 +1,44 @@
 from preprocess.images import load_image
 from preprocess.tokenizer import mark_end, mark_start
-from keras.utils.np_utils import np
+from keras import backend as K
 
 
 def generate_caption(image_path,
-                     image_model_transfer,
-                     decoder_model,
                      tokenizer,
-                     img_size,
+                     stylenet,
+                     mode='factual',
+                     seq2seq=None,
                      max_tokens=30):
+
+    if mode != 'factual':
+        assert stylenet is not None
+        assert seq2seq.mode == mode
 
     token_start = tokenizer.word_index[mark_start.strip()]
     token_end = tokenizer.word_index[mark_end.strip()]
 
+    img_size = K.int_shape(stylenet.model_encoder.input)[1:3]
+
     image = load_image(image_path, size=img_size)
-    image_batch = np.expand_dims(image, axis=0)
-    transfer_values = image_model_transfer.predict(image_batch)
 
-    shape = (1, max_tokens)
-    decoder_input_data = np.zeros(shape=shape, dtype=np.int)
+    output_tokens, transfer_values = stylenet.predict(
+        image=image,
+        token_start=token_start,
+        token_end=token_end,
+        max_tokens=max_tokens)
 
-    token_int = token_start
+    if mode != 'factual':
+        output_tokens = seq2seq.predict(
+            transfer_values=transfer_values,
+            input_tokens=output_tokens,
+            token_start=token_start,
+            token_end=token_end,
+            max_tokens=max_tokens)
+
     output_text = ''
-    count_tokens = 0
+    for token in output_tokens:
+        output_text += tokenizer.token_to_word(token) + ' '
+    output_text.replace(mark_start, '')
+    output_text.replace(mark_end, '')
 
-    while token_int != token_end and count_tokens < max_tokens:
-        decoder_input_data[0, count_tokens] = token_int
-        x_data = {
-            'transfer_values_input': transfer_values,
-            'decoder_input': decoder_input_data
-        }
-        decoder_output = decoder_model.predict(x_data)
-
-        token_onehot = decoder_output[0, count_tokens, :]
-        token_int = np.argmax(token_onehot)
-        sampled_word = tokenizer.token_to_word(token_int)
-
-        output_text += " " + sampled_word
-
-        count_tokens += 1
-
-    output_tokens = decoder_input_data[0]
-    output_text = output_text.replace(mark_end, '')
-    output_text = output_text.strip()
-
-    return image, output_tokens, output_text
+    return image, output_text

c979c825615a44a9c71edc8c443686502e7cfa6c
Dery Rahman A <dery.ra@gmail.com>
2019-04-25 16:32:20 +0700

    feat(seq2seq): add mode attention mechanism

--- nn/__init__.py
+++ nn/__init__.py
@@ -1,4 +1,6 @@
 from .factored_lstm import FactoredLSTM, FactoredLSTMCell
+from .attention import AttentionLayer
 
 FactoredLSTM = FactoredLSTM
 FactoredLSTMCell = FactoredLSTMCell
+AttentionLayer = AttentionLayer
